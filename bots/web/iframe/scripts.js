4abdf951077160d9ba4691dc30203e03d1c65912
ariel1985 <rothemariel@gmail.com>
2023-11-20 23:20:45 +0200

    change some style

--- bots/web/iframe/scripts.js
+++ bots/web/iframe/scripts.js
@@ -18,39 +18,34 @@ function createRoZChat() {
     box.style.backgroundColor = 'white'; // Set the background color
     box.style.color = 'black'; // Set the text color
     box.style.border = '1px solid black'; // Add a border
-    box.style.borderRadius = '10px';
+    // box.style.borderRadius = '10px';
     box.style.zIndex = '999'; // Ensure it appears below the floating image if they overlap
-    box.style.display = 'none'; // Start with the box hidden
+    box.style.display = 'block'; // Start with the box hidden
     box.style.justifyContent = 'center'; // Center horizontally
     box.style.alignItems = 'center'; // Center vertically
-    box.style.display = 'none';
+    box.style.zIndex = '1100'; // Ensure it's above the box
     // box.textContent = 'Box Content'; // Add some text to the box
 
     // Create the 'X' button
     var closeButton = document.createElement('div');
-    closeButton.textContent = 'X'; // Text for the close button
+    closeButton.textContent = 'ⓧ'; // Text for the close button
     closeButton.style.position = 'absolute';
     closeButton.style.top = '0';
     closeButton.style.left = '0';
-    closeButton.style.padding = '10px';
+    closeButton.style.padding = '0.25rem';
     closeButton.style.cursor = 'pointer'; // Change the mouse pointer on hover
-    closeButton.style.fontSize = '1vw'; // Relative font size
+    closeButton.style.fontSize = '2vw'; // Relative font size
+    closeButton.style.fontWeight = 'bold'; // Make the text bold
     closeButton.style.zIndex = '1001'; // Ensure it's above the box
+    closeButton.style.color = 'white';
+    closeButton.style.backgroundColor = 'black';
 
     // Append the close button to the box
     box.appendChild(closeButton);
 
-    // width: 95%;
-    // height: calc(100% - 2.5rem);
-    // border: none;
-    // display: block;
-    // bottom: 0;
-    // position: absolute;
-
     // Create an iframe to embed Google
     var iframe = document.createElement('iframe');
-    // iframe.src = 'https://main--steady-tarsier-ea3724.netlify.app/#/';
-    iframe.src = 'localhost:9000/#/'
+    iframe.src = 'https://main--steady-tarsier-ea3724.netlify.app/#/';
     iframe.style.width = '95%'; // Take the full width of the box
     iframe.style.height = 'calc(100% - 2.5rem)'; // Take the full height of the box minus the height of the close button
     iframe.style.border = 'none'; // Optional: remove the border of the iframe
@@ -71,11 +66,12 @@ function createRoZChat() {
             box.style.bottom = 'auto'; // Disable the bottom property
         } else {
             box.style.width = '20rem'; // Set a fixed width for desktop
-            box.style.height = '50vh'; // Set a fixed height for desktop
+            box.style.height = '100vh'; // Set a fixed height for desktop
             box.style.top = 'auto';
-            box.style.bottom = '9rem';
-            box.style.right = '4rem';
+            box.style.bottom = '0';
+            box.style.right = '0';
         }
+
     }
 
     // Initial box size update
