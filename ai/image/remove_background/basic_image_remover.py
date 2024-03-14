a6bd51787482993143b69764fe7918826e5ecd9c
ariel1985 <rothemariel@gmail.com>
2024-03-14 14:44:45 +0200

    playing with ai

--- ai/image/remove_background/basic_image_remover.py
+++ ai/image/remove_background/basic_image_remover.py
@@ -1,10 +1,12 @@
 from rembg import remove
 
-input_path = 'images/nobackground/rmbg_fractal.png'
-output_path = 'images/nobackground/rmbg_fractal2.png'
+input_path = 'images/hasbackground/m3-gpt-logo.png'
+output_path = 'images/nobackground/rmbg_m3-gpt-logo.png'
+print('Input:', input_path)
 
 with open(input_path, 'rb') as i:
     with open(output_path, 'wb') as o:
         input = i.read()
         output = remove(input)
         o.write(output)
+        print('Output:', output_path)
