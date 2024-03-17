3b6f20a131afaecddddaae8ecaf0dfbbfb7da410
ariel1985 <rothemariel@gmail.com>
2024-03-17 21:10:22 +0200

    init basic bot

---
+++ bots/telegram/webhooks/README.md
@@ -0,0 +1,10 @@
+# Telegram Bot Webhook
+
+POC for implementing webhooks in telegram bot. 
+
+# Set up
+
+```bash
+python -m venv venv
+python main.py
+```
---
+++ bots/telegram/webhooks/main.py
@@ -0,0 +1,28 @@
+# POC for implementing webhooks in telegram bot. 
+
+# env variables from dotenv
+import os
+from dotenv import load_dotenv
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_API_KEY')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
