06486ffaffae1bc9772cdda74067ded758b16c8a
ariel1985 <rothemariel@gmail.com>
2024-03-22 04:47:52 +0200

    uvicorn web server with ngrok tunneling running fastspi

--- bots/telegram/fastapi_webhooks/main.py
+++ bots/telegram/fastapi_webhooks/main.py
@@ -1,50 +1,39 @@
+
 # main.py
 
-from contextlib import asynccontextmanager
-from http import HTTPStatus
-from telegram import Update
-from telegram.ext import Application, CommandHandler
-from telegram.ext._contexttypes import ContextTypes
-from fastapi import FastAPI, Request, Response
-# env variables using dotenv
+# web framework for building APIs with Python
+from fastapi import FastAPI
+# local tunneling service for exposing local servers to the internet 
+from pyngrok import ngrok, conf
+# web server for ASGI applications
+import uvicorn
+
+# environment variable dotenv
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
-TOKEN = os.getenv('TELEGRAM_BOT_API_KEY')
+
+NGROK_AUTH_KEY = os.getenv("NGROK_AUTH")
+PORT = os.getenv("NGROK_PORT")
+conf.get_default().auth_token = NGROK_AUTH_KEY
+
+print('NGROK_AUTH', NGROK_AUTH_KEY)
+print('PORT', PORT)
 
 
-# Initialize python telegram bot
-ptb = (
-    Application.builder()
-    .updater(None)
-    .token(TOKEN) # replace <your-bot-token>
-    .read_timeout(7)
-    .get_updates_read_timeout(42)
-    .build()
-)
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    await ptb.bot.setWebhook(HTTP_SERVER_WEBHOOK_URL)
-    async with ptb:
-        await ptb.start()
-        yield
-        await ptb.stop()
 
-# Initialize FastAPI app (similar to Flask)
-app = FastAPI(lifespan=lifespan)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-@app.post("/")
-async def process_update(request: Request):
-    req = await request.json()
-    update = Update.de_json(req, ptb.bot)
-    await ptb.process_update(update)
-    return Response(status_code=HTTPStatus.OK)
 
-# Example handler
-async def start(update, _: ContextTypes.DEFAULT_TYPE):
-    """Send a message when the command /start is issued."""
-    await update.message.reply_text("starting...")
+# Open a ngrok tunnel to the uvicorn server
+http_tunnel = ngrok.connect(PORT, bind_tls=True) # bind_tls for https
+public_url = http_tunnel.public_url
+print("Public URL:", public_url)
 
-ptb.add_handler(CommandHandler("start", start))
+
+# Now you can access the FastAPI server over the internet via the ngrok tunnel
+uvicorn.run(app, host="0.0.0.0", port=8000)
