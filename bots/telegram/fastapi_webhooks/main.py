7fbf5d56142257fba2706d8ff81a52e2734c704f
ariel1985 <rothemariel@gmail.com>
2024-03-27 15:37:46 +0200

    check and set webhook with asyncio and httpx

--- bots/telegram/fastapi_webhooks/main.py
+++ bots/telegram/fastapi_webhooks/main.py
@@ -3,7 +3,8 @@
 
 # web framework for building APIs with Python
 from fastapi import FastAPI
-# local tunneling service for exposing local servers to the internet 
+
+import httpx
 
 # environment variable dotenv
 from dotenv import load_dotenv
@@ -11,14 +12,78 @@ import os
 
 load_dotenv()
 
+
+# ---- API ----
+
 # web framework for building APIs with Python
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/webhook/{token}")
+def read_root():
+    return {"Hello": "Webhook token"}
 
-if __name__ == "__main__":
+
+# ---- Telegram ----
+
+HOST_URL = os.getenv("TELEGRAM_WEBHOOK_URL")
+PORT = os.getenv("TELEGRAM_WEBHOOK_PORT")
+TOKEN = os.getenv("TELEGRAM_BOT_API_KEY")
+# webhook_security = OnlyTelegramNetworkWithSecret(real_secret="your-secret-from-config-or-env")
+# TODO: possible upgrades
+#  - move {secret} to path
+#  - use OnlyTelegramNetworkWithSecret as dependency:
+
+TELEGRAM_BOT_URL = f"https://api.telegram.org/bot{TOKEN}"
+TELEGRAM_SET_WEBHOOK_URL = f"{TELEGRAM_BOT_URL}/setWebhook"
+TELEGRAM_GET_WEBHOOK_INFO_URL = f"{TELEGRAM_BOT_URL}/getWebhookInfo"
+# TELEGRAM_SEND_MESSAGE_URL = f"{TELEGRAM_BOT_URL}/sendMessage"
+
+"""
+An asynchronous function that sends a POST request 
+to a specified URL with a given payload.
+"""
+async def request_json(url, payload):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload)
+    return response
+
+async def get_telegram_webhook_info():
+    res = await request_json(TELEGRAM_GET_WEBHOOK_INFO_URL, {})
+    print("response: ", res.json())
+    return res.json()
+
+async def isset_telegram_webhook_url(webhook_url) -> bool:
+    # check if the webhook is already set
+    webhook_info = await get_telegram_webhook_info()
     
+    print(f"Webhook info: {webhook_info['result']['url']}")
+    
+    return webhook_info['ok'] and webhook_info['result']['url'] == webhook_url
+
+async def set_telegram_webhook_url() -> bool:
+    payload = {"url": f"{HOST_URL}/webhook/{TOKEN}"}
+    print("payload: ", payload)
+    res = await request_json(TELEGRAM_SET_WEBHOOK_URL, payload)
+    print("\n\n req: ", res, "\n\n")
+    return res.status_code == 200
+
+async def main():
+    webhook_url = f"{HOST_URL}/webhook/{TOKEN}"
+    print("webhook_url to check: ", webhook_url)
+    
+    is_exsits_webhook = await isset_telegram_webhook_url(webhook_url)
+    print("is_exsits_webhook: ", "Yes" if is_exsits_webhook else "No")
+    
+    if not is_exsits_webhook:
+        # setting the telegram webhook url
+        status = await set_telegram_webhook_url()
+        print("telegram webhook status: ", status)
+
+    
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
--- bots/telegram/fastapi_webhooks/test.py
+++ bots/telegram/fastapi_webhooks/test.py
@@ -1,35 +1,4 @@
-import asyncio, logging, ngrok, os, uvicorn
-
-
-logging.basicConfig(level=logging.INFO)
-
-
-async def app(scope, receive, send):
-    assert scope["type"] == "http"
-
-    await send(
-        {
-            "type": "http.response.start",
-            "status": 200,
-            "headers": [[b"content-type", b"text/plain"]],
-        }
-    )
-    await send(
-        {
-            "type": "http.response.body",
-            "body": b"Hello, world!",
-        }
-    )
-
-
-if os.name == "nt":  # windows
-
-    async def setup():
-        listener = await ngrok.default()
-        listener.forward("localhost:8000")
-
-    asyncio.run(setup())
-    uvicorn.run(app=app)
-    exit()
-
-uvicorn.run(app=app, fd=ngrok.fd())
+# write a test to:
+# 1. test if hook is registered
+# 2. test if hook callback is called for authentication
+# 3. test if hook callback is called for ....
