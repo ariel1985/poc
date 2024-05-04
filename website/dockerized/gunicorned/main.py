2a81cf1c19697a12c4b592c33e3c1cd0c48566ef
ariel1985 <rothemariel@gmail.com>
2024-05-04 20:17:05 +0300

    move the folders to new dockerizedd dir

--- website/dockerized/gunicorned/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root() -> dict:
-    """Reads the root path and returns a dictionary"""
-    return {"Hello": "World......"}
