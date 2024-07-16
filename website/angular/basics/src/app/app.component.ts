38cd92339ffbb801842ad231a189806dc4d501b7
ariel1985 <rothemariel@gmail.com>
2024-07-16 12:26:54 +0300

    typo fix

--- website/angular/basics/src/app/app.component.ts
+++ website/angular/basics/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, m],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
